4561c85395288c94203913f024bd1c852e32995e
Nithish mee <nithishmee>
2024-01-20 15:59:39 +0530

    i added the division

---
+++ arith.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a=5,b=3,c;
+    c=a-b;
+    printf("the subtraction: %d",c);
+    c=a+b;
+    printf("the addition: %d",c);
+    c=a*b;
+    printf("the multiplicaton: %d",c);
+    c=a/b;
+    printf("the division: %d",c);
+    
+
+}
